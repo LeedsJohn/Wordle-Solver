100a2782da17e834898f79c6d3db5d840a5ee82a
John Leeds <leedsjohn6@gmail.com>
2022-05-08 15:15:09 -0700

    Initialized Guess.h

---
+++ Guess.h
@@ -0,0 +1,57 @@
+// John Leeds
+// 5/8/2022
+// Guess.h
+// Header file for class Guess
+
+#ifndef GUESS_HDR
+#define GUESS_HDR
+#include <string>
+
+class Guess{
+    public:
+        /*
+        Constructor
+        */
+        Guess();
+
+        /*
+        inputInfo
+        Gets user input and updates word and result
+        */
+        void inputInfo();
+
+        /*
+        getResult
+        Returns result
+        */
+        std::string& getResult() const;
+
+        /*
+        getWord
+        Returns word
+        */
+        std::string& getWord() const;
+
+    private:
+        std::string result;
+        std::string word;
+
+        /*
+        inputResult
+        Gets input from the user reflecting the result from the previous
+        guess.
+        Enter one character for each character - 
+            g for green,
+            y for yellow,
+            x for gray
+        */
+        std::string& inputResult() const;
+
+        /*
+        inputWord
+        Gets the last word
+        */
+        std::string& inputWord() const;
+};
+
+#endif
