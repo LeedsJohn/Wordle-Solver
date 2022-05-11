4c0d93294430ce682bcbd26d30a661a3ddacdd13
John Leeds <leedsjohn6@gmail.com>
2022-05-10 21:07:54 -0700

    First draft of checkWord

--- Word.cpp
+++ Word.cpp
@@ -4,6 +4,8 @@
 // Implementation file for class Word
 
 #include <string>
+#include <set>
+#include <iterator>
 #include "CharInfo.h"
 #include "Word.h"
 
@@ -11,7 +13,8 @@
 Constructor
 Receives a 5 letter word
 */
-Word::Word(std::string& w){
+Word::Word(std::string &w)
+{
     word = w;
 }
 
@@ -21,7 +24,12 @@ Receives CharInfo
 Returns true if the word could be a possible answer
 Else, false
 */
-bool Word::checkWord(CharInfo charinfo) const;
+bool Word::checkWord(CharInfo charinfo) const
+{
+    return ( checkGreens(charinfo.getGreens() ||
+                checkYellows(charinfo.getYellows() ||
+                checkGrays(charinfo.getGrays(), charinfo.getYellows());
+}
 
 /*
 getWordScore
@@ -35,6 +43,69 @@ int Word::getWordScore(CharInfo charinfo) const;
 getWord
 Returns the word
 */
-std::string Word::getWord() const{
+std::string Word::getWord() const
+{
     return word;
+}
+
+/*
+checkGreen
+Helper function for update
+Returns true if a word should be eliminated based on having
+an incorrect character in a green position
+*/
+bool Word::checkGreens(char greens[5])
+{
+    for (int i = 0; i < 5; i++)
+    {
+        if (greens[i] != '*' && greens[i] != word[i])
+        {
+            return true;
+        }
+    }
+    return false;
+}
+
+/*
+checkYellow
+Helper function for update
+Returns true if
+*/
+bool Word::checkYellows(std::set<char> yellows)
+{
+    std::set<char>::iterator itr;
+    for (itr = yellows.begin(); itr != yellows.end(); itr++)
+    {
+        bool flag = false;
+        for (int i = 0; i < 5; i++)
+        {
+            if (word[i] == *itr)
+            {
+                flag = true;
+            }
+        }
+        if (!flag)
+        {
+            return false;
+        }
+    }
+    return true;
+}
+
+/*
+checkGrays
+Helper function for update
+Returns true if a character is in grays but not in yellows
+*/
+bool Word::checkGrays(std::set<char> grays, std::set<char> yellows)
+{
+    for (int i = 0; i < 5; i++)
+    {
+        if (grays.find(word[i]) != grays.end() &&
+            yellows.find(word[i]) == yellows.end())
+        {
+            return true;
+        }
+    }
+    return false;
 }
--- Word.h
+++ Word.h
@@ -6,40 +6,64 @@
 #ifndef WORD_HDR
 #define WORD_HDR
 #include <string>
+#include <set>
 #include "CharInfo.h"
 
-class Word{
-    public:
-        /*
-        Constructor
-        Receives a 5 letter word
-        */
-        Word(std::string& w);
+class Word
+{
+public:
+    /*
+    Constructor
+    Receives a 5 letter word
+    */
+    Word(std::string &w);
 
-        /*
-        checkWord
-        Receives CharInfo
-        Returns true if the word could be a possible answer
-        Else, false
-        */
-        bool checkWord(CharInfo charinfo) const;
-        
-        /*
-        getWordScore
-        Receives CharInfo
-        Returns the score of the word based off of how many
-        potential green, yellow, and gray letters it has
-        */
-        int getWordScore(CharInfo charinfo) const;
+    /*
+    checkWord
+    Receives CharInfo
+    Returns true if the word could be a possible answer
+    Else, false
+    */
+    bool checkWord(CharInfo charinfo) const;
 
-        /*
-        getWord
-        Returns the word
-        */
-        std::string getWord() const;
+    /*
+    getWordScore
+    Receives CharInfo
+    Returns the score of the word based off of how many
+    potential green, yellow, and gray letters it has
+    */
+    int getWordScore(CharInfo charinfo) const;
 
-    private:
-        std::string word;
+    /*
+    getWord
+    Returns the word
+    */
+    std::string getWord() const;
+
+private:
+    std::string word;
+
+    /*
+    checkGreen
+    Helper function for update
+    Returns true if a word should be eliminated based on having
+    an incorrect character in a green position
+    */
+    bool checkGreens(char greens[5]);
+
+    /*
+    checkYellow
+    Helper function for update
+    Returns true if
+    */
+    bool checkYellows(std::set<char> yellows);
+
+    /*
+    checkGrays
+    Helper function for update
+    Returns true if a character is in grays but not in yellows
+    */
+    bool checkGrays(std::set<char> grays, std::set<char> yellows);
 };
 
 #endif
